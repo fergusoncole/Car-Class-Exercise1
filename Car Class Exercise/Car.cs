fe29897f7536776b160e5c8331b223c860db378b
Cole Ferguson <fergusoncole98@outlook.com>
2022-07-17 17:01:19 -0500

    Initial Commit

---
+++ Car Class Exercise/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Class_Exercise
+{
+    public class Car
+    {
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }   
+    }
+}
---
+++ Car Class Exercise/Program.cs
@@ -0,0 +1,10 @@
+﻿using Car_Class_Exercise;
+
+var car1 = new Car();
+car1.Year = 1998;
+car1.Make = "Ford";
+car1.Model = "F-150";
+
+Console.WriteLine($"Year: {car1.Year}");
+Console.WriteLine($"Make: {car1.Make}");
+Console.WriteLine($"Year: {car1.Model}");
